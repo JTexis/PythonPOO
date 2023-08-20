cfd99065cf73e4c4b3ec21d0ea932d0b443ee039
JavierTexis <robertotsux@gmail.com>
2023-08-20 15:50:09 -0700

    Adding grades.py module

---
+++ Practice1/Grades.py
@@ -0,0 +1,15 @@
+AlumnsNames = []
+AlumnsGrades = []
+
+average = 0
+highest = 0
+lowest = 0
+
+option = 0
+totalCal = 0
+
+while True:
+    print('OPTIONS:')
+    print('Capture a')
+    print('HI')
+    print('Hello')
