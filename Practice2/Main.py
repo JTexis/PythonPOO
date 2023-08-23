68caa21b60917c9b5c87281fe7a1b5d00c5b4463
JavierTexis <robertotsux@gmail.com>
2023-08-22 21:02:34 -0700

    Implementing CSV file storage functionality

--- Practice2/Main.py
+++ Practice2/Main.py
@@ -1,7 +1,8 @@
 from Student import Student
-
+import csv
 
 Students = []
+
 
 Students.append(Student('Roberto', 1272185, 'POO', 69))
 Students.append(Student('Juan', 1272185, 'Maths', 80 ))
@@ -10,5 +11,18 @@ Students.append(Student('Ricardo', 1274895, 'Spanish', 78))
 Students.append(Student('Sergio', 1274935, 'Automation', 98))
 Students.append(Student('Zen', 1234857, 'Advanced Electronic', 77))
 
-for student in Students:
-    student.printStatus()
+Students.append(Student(str(input('Write your name: ')),
+                        int(input('Insert the tuition: ')),
+                        str(input('Type the subject: ')),
+                        int(input('Type the final grade: '))))
+
+with open('Practice2\Students.csv', 'a', newline='') as file:
+
+    dictKeys = vars(Students[0]).keys()
+    listKeys = list(dictKeys)
+    writer = csv.DictWriter(file, fieldnames=listKeys)
+
+    for student in Students:
+        writer.writerow(vars(student))
+
+
