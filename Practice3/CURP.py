e3bc719aaaffd7aaf65196ecdb93c00b489219dd
JavierTexis <robertotsux@gmail.com>
2023-08-24 15:27:18 -0700

    Implementing CURP functionalities

--- Practice3/CURP.py
+++ Practice3/CURP.py
@@ -1,3 +1,5 @@
+from utils import *
+
 class CURP():
 
     def __init__(self, name:str, gender:str, birthdate:str, entity:str):
@@ -7,4 +9,18 @@ class CURP():
         self.entity = entity
 
     def generateCURP(self):
-        return 'curp'
+        curplist = []
+        #First 4 letters
+        curplist.append(first4())
+        #Adding birth date letters
+        curplist.append(second6())
+        #Adding gender
+        curplist.append(third1())
+        #Adding federal entity
+        curplist.append(fourth2())
+        #Adding consonants
+        curplist.append(fifth3())
+        #Adding random code
+        curplist.append(sixth2())
+        curpstr = ''.join(curplist)
+        return curpstr
---
+++ Practice3/utils.py
@@ -0,0 +1,17 @@
+def first4():
+    return 'a'
+
+def second6():
+    return 'b'
+
+def third1():
+    return 'c'
+
+def fourth2():
+    return 'd'
+
+def fifth3():
+    return 'e'
+
+def sixth2():
+    return 'f'
